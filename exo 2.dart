5a74f64c749a256eb111a2394b7c5ee2a9c4f7bc
massinissa mameri <massilockwood93@gmail.com>
2025-11-12 17:50:19 -0500

    Q3-4

--- exo 2.dart
+++ exo 2.dart
@@ -2,4 +2,12 @@ class point {
   double _x, _y;
   point(this._x, this._y);
   
+  void move(double dx, double dy){
+    _x += dx;
+    _y += dy;
+  }
+
+void display() {
+  print("point(x: $_x, y: $_y)");
+}
 }
