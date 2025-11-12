842edc68ca23f7c3b07f1dc36acb2db73a6ecbd8
massinissa mameri <massilockwood93@gmail.com>
2025-11-12 18:09:36 -0500

    Q6

--- exo 3.dart
+++ exo 3.dart
@@ -4,6 +4,14 @@ class compte {
   compte(this.numerocompte, [this._solde = 0.0]);
 
   String get solde => "${_solde.toStringAsFixed(2)}  \$";
+
+  void depot(double montant) {
+    if (montant > 0) {
+      _solde += montant;}
+      else {
+        print("montant invalide : $montant");
+      }
+    }
+  }
+
   
-  
-}
