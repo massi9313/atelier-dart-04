8b47ba91c1acf2d81b6a2a3f89fb8d855d115b8e
massinissa mameri <massilockwood93@gmail.com>
2025-11-12 17:25:06 -0500

    Q4-5

--- exo 1.dart
+++ exo 1.dart
@@ -1,4 +1,10 @@
 class couleur {
   final int r, g, b;
   couleur(this.r, this.g, this.b);
+  couleur.rouge() : this(255, 0, 0);
+  couleur.vert() : this(0, 255, 0);
+  couleur.bleu() : this(0, 0, 255);
+  void affichercouleur() {
+    print("(R : $r, V : $g, B : $b)");
+  }
 }
