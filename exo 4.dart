8563984732e35265754f714e49eec98e063cb5a8
massinissa mameri <massilockwood93@gmail.com>
2025-11-12 18:50:01 -0500

    Q4-6

--- exo 4.dart
+++ exo 4.dart
@@ -7,3 +7,13 @@ class Media{
     print("ceci est un media generique.");
   }
 }
+
+class Livre extends Media {
+  final String auteur;
+   Livre(String titre, this.auteur) : super(titre);
+
+@override
+   void affichertype() {
+    print("ceci est un livre : $titre par $auteur. ");
+   }
+  }
