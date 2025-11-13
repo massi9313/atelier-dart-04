29ee99df7a145a9552186be7683c0568c3ac024a
massinissa mameri <massilockwood93@gmail.com>
2025-11-12 21:20:31 -0500

    Q5

--- exo 7.dart
+++ exo 7.dart
@@ -6,3 +6,10 @@ class tache {
     nombreTotal++;
   }
 }
+
+void main () {
+  var t1 = tache("faire les devoirs");
+  var t2 = tache("aller au gym");
+  var t3 = tache("preparer le repas");
+  print("nombre total de taches : ${tache.nombreTotal}");
+}
