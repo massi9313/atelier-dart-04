600751c5981a1ab28fca82c7f583186395e4d271
massinissa mameri <massilockwood93@gmail.com>
2025-11-12 19:11:12 -0500

    Q4-5

--- exo 5.dart
+++ exo 5.dart
@@ -5,3 +5,13 @@ abstract class forme {
     print("calcul de l'aire ...");
   }
 }
+
+class cercle extends forme {
+  final double rayon;
+  cercle(this.rayon);
+
+  @override
+  double calculerAire() {
+    return 3.1416 * rayon * rayon;
+  }
+  }
