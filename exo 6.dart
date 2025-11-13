3d54b1263dff757133bba59fe34ce111691e2b44
massinissa mameri <massilockwood93@gmail.com>
2025-11-12 19:24:04 -0500

    Q3-5

--- exo 6.dart
+++ exo 6.dart
@@ -2,3 +2,14 @@ class connnectable {
   void connecter(String utilisateur) {}
   void deconnecter() {}
 }
+
+class ServeurApi implements connnectable {
+  @override
+  void connecter(String utilisateur) {
+    print("ServeurApi : connexion etablie pour $utilisateur.");
+  }
+  @override
+  void deconnecter() {
+    print("ServeurApi : deconnexion reussie .");
+  }
+}
