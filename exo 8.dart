981227cda53f5873c6ba8bd329e65c75b3fe899f
massinissa mameri <massilockwood93@gmail.com>
2025-11-12 22:01:08 -0500

    Q4-7

--- exo 8.dart
+++ exo 8.dart
@@ -1,10 +1,33 @@
-class livre {
+class Livre {
   String titre;
   String auteur;
+  int _pages = 200;
+  static int totalLivres = 0;
 
-  livre(this.titre, this.auteur);
+  Livre(this.titre, this.auteur) {
+    totalLivres++;
+  }
+
+  int get pages => _pages;
 
   void afficherInfos() {
-    print("titre: $titre, auteur: $auteur");
+    print("titre: $titre, auteur: $auteur, pages: $_pages");
   }
+
+  static void affichertotal() {
+    print("total de livres crees : $totalLivres");
+  }
+}
+
+void main() {
+  var livre1 = Livre("l'Etranger","Albert camus");
+  var livre2 = Livre("le Petit Prince", "Antoine de Saint-Exupery");
+  var livre3 = Livre("1948", "George Orwell");
+
+  livre1.afficherInfos();
+  livre2.afficherInfos();
+  livre3.afficherInfos();
+
+  Livre.affichertotal();
+
 }
